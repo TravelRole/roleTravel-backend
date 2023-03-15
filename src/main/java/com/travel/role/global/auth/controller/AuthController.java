faeaba0173510729ffc6426e9bfabc1a0fb1cb88
kwYoohae <gocksdl7979@gmail.com>
2023-03-15 15:15:57 +0900

    fix: PostMapping 누락 수정
    Co-authored-by: kwyoohae <gocksdl7979@gmail.com>
    Co-authored-by: cw-y <rina0405@naver.com>

--- src/main/java/com/travel/role/global/auth/controller/AuthController.java
+++ src/main/java/com/travel/role/global/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.travel.role.global.auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class AuthController {
 
 	private final AuthService authService;
 
+	@PostMapping("/signup")
 	public ResponseEntity<?> signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
 		return authService.signUp(signUpRequestDTO);
 	}
