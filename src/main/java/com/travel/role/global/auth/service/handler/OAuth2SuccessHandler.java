e2886aaf791881789b9798cee9eeb99c96f07b89
kwYoohae <gocksdl7979@gmail.com>
2023-03-30 16:34:29 +0900

    feat: refresh token DB에 저장하는 로직 생성

--- src/main/java/com/travel/role/global/auth/service/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/travel/role/global/auth/service/handler/OAuth2SuccessHandler.java
@@ -1,6 +1,7 @@
 package com.travel.role.global.auth.service.handler;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -10,8 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.travel.role.domain.user.dao.UserRepository;
+import com.travel.role.domain.user.domain.UserEntity;
 import com.travel.role.global.auth.dto.TokenMapping;
 import com.travel.role.global.auth.service.TokenProvider;
 
@@ -26,6 +30,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
 	private static final String redirectPath = "http://localhost:3000/landing/social";
 
+	private final UserRepository userRepository;
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
 		Authentication authentication) throws IOException, ServletException {
@@ -33,13 +39,25 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 	}
 
 	@Override
+	@Transactional
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException, ServletException {
 		TokenMapping token = tokenProvider.createToken(authentication);
+
+		saveUser(token);
 
 		response.setContentType("application/json");
 		response.setCharacterEncoding("utf-8");
 
 		response.sendRedirect(redirectPath + "?accessToken=" + token.getAccessToken());
+	}
+
+	private void saveUser(TokenMapping token) {
+		Optional<UserEntity> user = userRepository.findByEmail(token.getUserEmail());
+
+		if (user.isEmpty())
+			return;
+
+		user.get().updateRefreshToken(token.getRefreshToken());
 	}
 }
