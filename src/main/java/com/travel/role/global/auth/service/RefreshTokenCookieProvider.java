09f60b8bdba1a90a28d249c0a5b1ae0cbdeeb92d
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2023-04-06 20:57:59 +0900

    feat: 배포가 되어 민감한값 환경변수 설정 및 쿠키 도메인 설정 (#21)

--- src/main/java/com/travel/role/global/auth/service/RefreshTokenCookieProvider.java
+++ src/main/java/com/travel/role/global/auth/service/RefreshTokenCookieProvider.java
@@ -2,19 +2,28 @@ package com.travel.role.global.auth.service;
 
 import java.time.Duration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.server.Cookie;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:application-oauth.yml")
 public class RefreshTokenCookieProvider {
 
 	public static final String REFRESH_TOKEN = "refreshToken";
-	private final long EXPIRED_TIME = 60 * 60 * 24 * 14;
+
+	@Value("${cookieExpireTime}")
+	private long EXPIRED_TIME;
+
+	@Value("${cookieDomain}")
+	private String COOKIE_DOMAIN;
 
 	public ResponseCookie createCookie(final String refreshToken) {
 		return ResponseCookie.from(REFRESH_TOKEN, refreshToken)
 			.path("/") // 모든 경로일 때
+			.domain(COOKIE_DOMAIN)
 			.maxAge(EXPIRED_TIME)
 			.build();
 	}
