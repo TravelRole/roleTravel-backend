02330d15edc9d2b1d485e97237943830b47c700e
kwYoohae <gocksdl7979@gmail.com>
2023-04-10 16:35:25 +0900

    faet: 방 만들기 위한 Request dto 생성

---
+++ src/main/java/com/travel/role/domain/room/dto/MakeRoomRequestDTO.java
@@ -0,0 +1,22 @@
+package com.travel.role.domain.room.dto;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+@Data
+public class MakeRoomRequestDTO {
+	private String roomName;
+
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate travelStartDate;
+
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate travelEndDate;
+
+	private Integer totalParticipants;
+
+	private String location;
+}
