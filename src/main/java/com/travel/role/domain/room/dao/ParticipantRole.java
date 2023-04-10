a95ec3ac97e3a36a4c7c507430f2a3a3d0a5d22d
kwYoohae <gocksdl7979@gmail.com>
2023-04-10 16:19:14 +0900

    feat: JPA Repository 생성

---
+++ src/main/java/com/travel/role/domain/room/dao/ParticipantRole.java
@@ -0,0 +1,6 @@
+package com.travel.role.domain.room.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRole extends JpaRepository<ParticipantRole, Long> {
+}
