47c22b622c7980f5e8b2346d0d1da9b83f33c43a
kwYoohae <gocksdl7979@gmail.com>
2023-04-10 23:26:18 +0900

    feat: Status 코드 변경

--- src/main/java/com/travel/role/domain/room/controller/RoomController.java
+++ src/main/java/com/travel/role/domain/room/controller/RoomController.java
@@ -1,9 +1,11 @@
 package com.travel.role.domain.room.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.travel.role.domain.room.dto.MakeRoomRequestDTO;
@@ -19,6 +21,7 @@ public class RoomController {
 
 	private final RoomService roomService;
 
+	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/v1/room")
 	public void makeRoom(@AuthenticationPrincipal UserPrincipal userPrincipal,@RequestBody MakeRoomRequestDTO dto) {
 		roomService.makeRoom(userPrincipal, dto);
