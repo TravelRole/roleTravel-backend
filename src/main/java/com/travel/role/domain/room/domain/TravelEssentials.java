5b0069d51e28e0f40468f9dd04102d1939056954
Icln <gksmfthsus35@naver.com>
2023-04-11 13:37:23 +0900

    fix: updatable 옵션 추가

--- src/main/java/com/travel/role/domain/room/domain/TravelEssentials.java
+++ src/main/java/com/travel/role/domain/room/domain/TravelEssentials.java
@@ -23,11 +23,11 @@ public class TravelEssentials {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "room_id", nullable = false)
+    @JoinColumn(name = "room_id", nullable = false, updatable = false)
     private Room room;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
 
     @Column(length = 20, nullable = false)
