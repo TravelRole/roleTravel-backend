b956d124e02df8fb1d742c0ed79e1e9ff30cc4ae
kwYoohae <gocksdl7979@gmail.com>
2023-04-11 22:08:58 +0900

    fix: NotBlank를 실행할떄, Formatting이 되어있지 않은걸 보고 오류를 내는 로직 수정

--- src/main/java/com/travel/role/domain/user/dto/ConfirmUserRequestDTO.java
+++ src/main/java/com/travel/role/domain/user/dto/ConfirmUserRequestDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -19,7 +20,7 @@ public class ConfirmUserRequestDTO {
 	@NotBlank(message = "값이 비어있으면 안됩니다.")
 	private String name;
 
-	@NotBlank(message = "값이 비어있으면 안됩니다.")
+	@NotNull(message = "값이 비어있으면 안됩니다.")
 	@JsonFormat(pattern = "yyyy/MM/dd")
 	private LocalDate birth;
 }
