e0279a52a2cdc4ca92d6f1e6b978892c9ca5198e
kwYoohae <gocksdl7979@gmail.com>
2023-04-11 21:58:44 +0900

    fix: RequestDTO 직렬화수정

--- src/main/java/com/travel/role/domain/user/dto/ConfirmUserRequestDTO.java
+++ src/main/java/com/travel/role/domain/user/dto/ConfirmUserRequestDTO.java
@@ -9,9 +9,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ConfirmUserRequestDTO {
 
 	@NotBlank(message = "값이 비어있으면 안됩니다.")
