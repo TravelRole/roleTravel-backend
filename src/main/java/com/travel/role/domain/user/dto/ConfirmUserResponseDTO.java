5aaad8a15d03a54dff7f2c63cbf1f64dad8813bc
kwYoohae <gocksdl7979@gmail.com>
2023-04-12 23:53:06 +0900

    feat: LocalDate 반환 형식 변경 요청 수정

--- src/main/java/com/travel/role/domain/user/dto/ConfirmUserResponseDTO.java
+++ src/main/java/com/travel/role/domain/user/dto/ConfirmUserResponseDTO.java
@@ -2,7 +2,10 @@ package com.travel.role.domain.user.dto;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,5 +18,7 @@ public class ConfirmUserResponseDTO {
 	private String message;
 	private HttpStatus httpStatus;
 	private String email;
+
+	@JsonFormat(pattern = "yyyy/MM/dd")
 	private LocalDate createdAt;
 }
