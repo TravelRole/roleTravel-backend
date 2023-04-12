d63566045b84852a06c1955d49be981f1ce22a33
guswls159357 <guswls159357@naver.com>
2023-04-13 00:36:39 +0900

    feat: 회원 프로필 조회 서비스 메서드 작성
    해당 메소드 내에서는 읽기 작업만 수행하므로
    Transactional 의 readOnly 속성을 true 로 설정함

--- src/main/java/com/travel/role/domain/user/service/UserService.java
+++ src/main/java/com/travel/role/domain/user/service/UserService.java
@@ -2,6 +2,8 @@ package com.travel.role.domain.user.service;
 
 import static com.travel.role.global.exception.ExceptionMessage.*;
 
+import com.travel.role.domain.user.dto.UserProfileDetailResDTO;
+import com.travel.role.domain.user.exception.UserInfoNotFoundException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +12,36 @@ import com.travel.role.domain.user.domain.User;
 import com.travel.role.domain.user.dto.UserProfileResponseDTO;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
-	private final UserRepository userRepository;
 
-	public UserProfileResponseDTO getBasicProfile(String email) {
-		User findUser = userRepository.findByEmail(email)
-			.orElseThrow(() -> new UsernameNotFoundException(
-				USERNAME_NOT_FOUND));
+    private final UserRepository userRepository;
 
-		return new UserProfileResponseDTO(findUser.getName(), findUser.getEmail(), findUser.getProfile());
-	}
+    @Transactional(readOnly = true)
+    public UserProfileResponseDTO getBasicProfile(String email) {
+        User findUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        USERNAME_NOT_FOUND));
+
+        return new UserProfileResponseDTO(findUser.getName(), findUser.getEmail(), findUser.getProfile());
+    }
+
+    @Transactional(readOnly = true)
+    public UserProfileDetailResDTO getUserProfile(String email) {
+
+        User findUser = findUserByEmailOrElseThrow(email);
+
+        return UserProfileDetailResDTO.fromUser(findUser);
+    }
+
+    private User findUserByEmailOrElseThrow(String email) {
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserInfoNotFoundException(
+                        USERNAME_NOT_FOUND));
+    }
 }
