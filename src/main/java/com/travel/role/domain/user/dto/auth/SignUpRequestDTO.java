3bb6158ca28065732fe4bf1f58f5b89e256b9b8e
Icln <gksmfthsus35@naver.com>
2023-04-14 01:04:32 +0900

    feat: 생성자 추가

--- src/main/java/com/travel/role/domain/user/dto/auth/SignUpRequestDTO.java
+++ src/main/java/com/travel/role/domain/user/dto/auth/SignUpRequestDTO.java
@@ -1,27 +1,29 @@
 package com.travel.role.domain.user.dto.auth;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.Data;
+import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpRequestDTO {
 
-	@NotBlank(message = "유저이름 값이 비어있으면 안됩니다.")
-	private String name;
+    @NotBlank(message = "유저이름 값이 비어있으면 안됩니다.")
+    private String name;
 
-	@NotBlank(message = "이메일 값이 비어있으면 안됩니다.")
-	@Email(message = "이메일 형식이 아닙니다.")
-	private String email;
+    @NotBlank(message = "이메일 값이 비어있으면 안됩니다.")
+    @Email(message = "이메일 형식이 아닙니다.")
+    private String email;
 
-	@NotBlank(message = "비밀번호 값이 비어있으면 안됩니다.")
-	private String password;
-	@JsonFormat(pattern = "yyyy/MM/dd")
-	private LocalDate birth;
+    @NotBlank(message = "비밀번호 값이 비어있으면 안됩니다.")
+    private String password;
+
+    @JsonFormat(pattern = "yyyy/MM/dd")
+    private LocalDate birth;
 }
