3f41044dec5d362b9416f18dd5c2306ddac13780
guswls159357 <guswls159357@naver.com>
2023-04-13 16:26:36 +0900

    feat: 회원 프로필 수정 컨트롤러 메서드 작성

--- src/main/java/com/travel/role/domain/user/controller/UserController.java
+++ src/main/java/com/travel/role/domain/user/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.travel.role.domain.user.controller;
 
+import javax.validation.Valid;
+
 import com.travel.role.domain.user.dto.UserProfileDetailResDTO;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.travel.role.domain.user.dto.UserProfileModifyReqDTO;
 import com.travel.role.domain.user.dto.UserProfileResponseDTO;
 import com.travel.role.domain.user.service.UserService;
 import com.travel.role.global.auth.token.UserPrincipal;
@@ -17,21 +23,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
-    private final UserService userService;
+	private final UserService userService;
 
-    @GetMapping("/basic-profile")
-    public ResponseEntity<UserProfileResponseDTO> basicProfile(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+	@GetMapping("/basic-profile")
+	public ResponseEntity<UserProfileResponseDTO> basicProfile(@AuthenticationPrincipal UserPrincipal userPrincipal) {
 
-        UserProfileResponseDTO resDTO = userService.getBasicProfile(userPrincipal.getEmail());
+		UserProfileResponseDTO resDTO = userService.getBasicProfile(userPrincipal.getEmail());
 
-        return ResponseEntity.ok(resDTO);
-    }
+		return ResponseEntity.ok(resDTO);
+	}
 
-    @GetMapping("/users")
-    public ResponseEntity<UserProfileDetailResDTO> getUserProfile(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+	@GetMapping("/users")
+	public ResponseEntity<UserProfileDetailResDTO> getUserProfile(
+		@AuthenticationPrincipal UserPrincipal userPrincipal) {
 
-        UserProfileDetailResDTO resDTO = userService.getUserProfile(userPrincipal.getEmail());
+		UserProfileDetailResDTO resDTO = userService.getUserProfile(userPrincipal.getEmail());
 
-        return ResponseEntity.ok(resDTO);
-    }
+		return ResponseEntity.ok(resDTO);
+	}
+
+	@PutMapping("/users")
+	public ResponseEntity<Void> modifyUserProfile(@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@RequestBody @Valid UserProfileModifyReqDTO reqDTO) {
+
+		userService.modifyUserProfile(userPrincipal.getEmail(), reqDTO);
+
+		return ResponseEntity.ok().build();
+	}
 }
