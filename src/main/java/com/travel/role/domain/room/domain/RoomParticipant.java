b98f23d096b4d19dbf4574bab9621fc9b34cfb9f
Icln <gksmfthsus35@naver.com>
2023-04-14 01:02:24 +0900

    feat: Getter 추가

--- src/main/java/com/travel/role/domain/room/domain/RoomParticipant.java
+++ src/main/java/com/travel/role/domain/room/domain/RoomParticipant.java
@@ -1,29 +1,19 @@
 package com.travel.role.domain.room.domain;
 
+import com.travel.role.domain.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import com.travel.role.domain.user.domain.User;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-
 @Table(name = "ROOM_PARTICIPANT_INFO")
 @Entity
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
