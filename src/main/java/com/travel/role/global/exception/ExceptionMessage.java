31efad688c8d704c8282da710b1dcdbb80fc172c
guswls159357 <guswls159357@naver.com>
2023-04-13 16:16:24 +0900

    feat: 회원 정보 데이터 관련 예외 메시지 작성
    회원 이름과 회원 생년월일에 관한 예외 메시지 작성

--- src/main/java/com/travel/role/global/exception/ExceptionMessage.java
+++ src/main/java/com/travel/role/global/exception/ExceptionMessage.java
@@ -1,6 +1,8 @@
 package com.travel.role.global.exception;
 
 public final class ExceptionMessage {
+
+	private ExceptionMessage(){}
 	public static final String NOT_EXISTS_TOKEN = "토큰 값이 존재하지 않습니다.";
 	public static final String USERNAME_NOT_FOUND = "해당하는 유저가 존재하지 않습니다.";
 	public static final String INVALID_TOKEN = "유효하지 않은 토큰입니다.";
@@ -8,7 +10,8 @@ public final class ExceptionMessage {
 	public static final String BAD_CREDENTIAL_USER = "인증정보가 다른 회원입니다.";
 	public static final String INVALID_USER = "유효하지 않은 회원입니다.";
 	public static final String MAIL_SEND_FAILD_ERROR = "서버가 메일을 보내는데 실패하였습니다.";
-
+	public static final String USERNAME_NOT_EMPTY = "이름은 필수값 입니다.";
+	public static final String BIRTH_PATTERN = "생년월일을 1900 ~ 2099 년 사이의 XXXX/XX/XX 형식으로 입력해 주세요";
 
 	// Room
 	public static final String INVALID_DATE_ERROR = "시작 날짜의 크기는 종료 날짜의 크기보다 클 수 없습니다.";
