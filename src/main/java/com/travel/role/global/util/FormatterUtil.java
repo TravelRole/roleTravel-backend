1aca00038c8d6af33c8904a7a4b72aa46d9db8d3
guswls159357 <guswls159357@naver.com>
2023-04-13 16:20:39 +0900

    feat: string 과 여러 데이터 타입을 변환할 수 있는 util 클래스 작성
    string 을 LocalDate 로 변환하는 메서드 작성
    이후, string 과 다른 데이터 타입을 변환할 때 필요한 메서드 작성 예정

---
+++ src/main/java/com/travel/role/global/util/FormatterUtil.java
@@ -0,0 +1,24 @@
+package com.travel.role.global.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import com.travel.role.domain.room.exception.InvalidLocalDateException;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public abstract class FormatterUtil {
+
+	public static LocalDate stringToLocalDate(String str, String pattern, String message) {
+
+		try {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+			return LocalDate.parse(str, formatter);
+		} catch (DateTimeParseException e) {
+			throw new InvalidLocalDateException(message);
+		}
+	}
+}
