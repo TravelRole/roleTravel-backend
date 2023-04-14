c405d9e16965ef74b190a910779a7f4ec208f95e
guswls159357 <guswls159357@naver.com>
2023-04-14 17:05:29 +0900

    feat: 비밀번호와 관련된 예외 메시지 작성
    추가로, 입력시 두 값이 일치하지 않는 경우 String.format() 으로
    두 속성 값을 넣어 사용할 수 있는 메시지도 작성

--- src/main/java/com/travel/role/global/exception/ExceptionMessage.java
+++ src/main/java/com/travel/role/global/exception/ExceptionMessage.java
@@ -8,10 +8,14 @@ public final class ExceptionMessage {
 	public static final String INVALID_TOKEN = "유효하지 않은 토큰입니다.";
 	public static final String ALREADY_EXIST_USER = "이미 존재하는 회원입니다.";
 	public static final String BAD_CREDENTIAL_USER = "인증정보가 다른 회원입니다.";
+	public static final String INVALID_PASSWORD = "비밀번호가 일치하지 않습니다.";
 	public static final String INVALID_USER = "유효하지 않은 회원입니다.";
 	public static final String MAIL_SEND_FAILD_ERROR = "서버가 메일을 보내는데 실패하였습니다.";
 	public static final String USERNAME_NOT_EMPTY = "이름은 필수값 입니다.";
-	public static final String BIRTH_PATTERN = "생년월일을 1900 ~ 2099 년 사이의 XXXX/XX/XX 형식으로 입력해 주세요";
+	public static final String PASSWORD_NOT_EMPTY = "비밀번호는 필수값 입니다.";
+	public static final String INPUT_VALUE_NOT_MATCH = "%s 값과 %s 값이 일치하지 않습니다.";
+	public static final String BIRTH_PATTERN = "생년월일을 1900 ~ 2099 년 사이의 XXXX/XX/XX 형식으로 입력해 주세요.";
+	public static final String PASSWORD_PATTERN = "비밀번호는 영문, 숫자, 특수문자(!, -, _)로 구성된 8-16 자로 입력해 주세요.";
 
 	// Room
 	public static final String INVALID_DATE_ERROR = "시작 날짜의 크기는 종료 날짜의 크기보다 클 수 없습니다.";
