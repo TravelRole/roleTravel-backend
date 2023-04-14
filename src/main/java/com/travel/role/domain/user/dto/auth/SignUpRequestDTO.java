691b0b3a3eb36d5a69b0639b237b8e0671714339
guswls159357 <guswls159357@naver.com>
2023-04-14 17:05:56 +0900

    fix: 비밀번호와 관련된 예외 메시지 작성으로 인한 변경

--- src/main/java/com/travel/role/domain/user/dto/auth/SignUpRequestDTO.java
+++ src/main/java/com/travel/role/domain/user/dto/auth/SignUpRequestDTO.java
@@ -21,7 +21,7 @@ public class SignUpRequestDTO {
 	@Email(message = "이메일 형식이 아닙니다.")
 	private String email;
 
-	@NotBlank(message = "비밀번호 값이 비어있으면 안됩니다.")
+	@NotBlank(message = ExceptionMessage.PASSWORD_NOT_EMPTY)
 	private String password;
 	@JsonFormat(pattern = "yyyy/MM/dd")
 	private LocalDate birth;
