4cd9f8e61a88cba1cfd9582823abc9605633f30f
Icln <gksmfthsus35@naver.com>
2023-04-15 21:06:56 +0900

    feat: 에러 상수 추가

--- src/main/java/com/travel/role/global/exception/ExceptionMessage.java
+++ src/main/java/com/travel/role/global/exception/ExceptionMessage.java
@@ -9,13 +9,18 @@ public final class ExceptionMessage {
 	public static final String ALREADY_EXIST_USER = "이미 존재하는 회원입니다.";
 	public static final String BAD_CREDENTIAL_USER = "인증정보가 다른 회원입니다.";
 	public static final String INVALID_USER = "유효하지 않은 회원입니다.";
-	public static final String MAIL_SEND_FAILD_ERROR = "서버가 메일을 보내는데 실패하였습니다.";
+	public static final String MAIL_SEND_FAILED_ERROR = "서버가 메일을 보내는데 실패하였습니다.";
 	public static final String USERNAME_NOT_EMPTY = "이름은 필수값 입니다.";
 	public static final String BIRTH_PATTERN = "생년월일을 1900 ~ 2099 년 사이의 XXXX/XX/XX 형식으로 입력해 주세요";
 	public static final String ROOM_NOT_FOUND = "해당하는 방이 존재하지 않습니다.";
-	public static final String VALUE_NOT_EMPTY = "값이 비어있으면 안됩니다.";
 	public static final String USER_NOT_PARTICIPATE_ROOM = "사용자는 방에 참여하지 않았습니다";
-
+	public static final String ROOM_ID_VALUE_NOT_EMPTY = "방 아이디는 필수값 입니다.";
+	public static final String PLACE_NAME_VALUE_NOT_EMPTY = "장소 이름은 필수값 입니다.";
+	public static final String PLACE_ADDRESS_VALUE_NOT_EMPTY = "장소 주소는 필수값 입니다.";
+	public static final String LATITUDE_VALUE_NOT_EMPTY = "위도는 필수값 입니다.";
+	public static final String LONGITUDE_NOT_EMPTY = "경도는 필수값 입니다.";
+	
+	
 	// Room
 	public static final String INVALID_DATE_ERROR = "시작 날짜의 크기는 종료 날짜의 크기보다 클 수 없습니다.";
 }
