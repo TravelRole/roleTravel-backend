4369a8a45b2bfe924c125f91f212326cf58db063
Icln <gksmfthsus35@naver.com>
2023-04-15 17:48:35 +0900

    fix: 반환타입 수정

--- src/main/java/com/travel/role/domain/room/controller/WantPlaceController.java
+++ src/main/java/com/travel/role/domain/room/controller/WantPlaceController.java
@@ -4,12 +4,9 @@ import com.travel.role.domain.room.dto.WantPlaceRequestDTO;
 import com.travel.role.domain.room.service.WantPlaceService;
 import com.travel.role.global.auth.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,8 +17,8 @@ public class WantPlaceController {
     private final WantPlaceService wantPlaceService;
 
     @PostMapping("/want-place")
-    public ResponseEntity<Void> addWantPlace(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestBody @Valid WantPlaceRequestDTO wantPlaceRequestDTO) {
+    @ResponseStatus(HttpStatus.OK)
+    public void addWantPlace(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestBody @Valid WantPlaceRequestDTO wantPlaceRequestDTO) {
         wantPlaceService.addWantPlace(userPrincipal, wantPlaceRequestDTO);
-        return ResponseEntity.ok().build();
     }
 }
