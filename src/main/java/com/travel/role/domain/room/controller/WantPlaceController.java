d0da6cc947344c4968bf761826ed3bc106d323f5
Icln <gksmfthsus35@naver.com>
2023-04-16 22:12:42 +0900

    feat: 가고싶은 장소 조회 api

--- src/main/java/com/travel/role/domain/room/controller/WantPlaceController.java
+++ src/main/java/com/travel/role/domain/room/controller/WantPlaceController.java
@@ -1,10 +1,13 @@
 package com.travel.role.domain.room.controller;
 
+
 import com.travel.role.domain.room.dto.WantPlaceRequestDTO;
+import com.travel.role.domain.room.dto.WantPlaceResponseDTO;
 import com.travel.role.domain.room.service.WantPlaceService;
 import com.travel.role.global.auth.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,12 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class WantPlaceController {
     private final WantPlaceService wantPlaceService;
+
+    @GetMapping("/want-place")
+    public ResponseEntity<WantPlaceResponseDTO> getRoomList(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam("id") Long id) {
+        WantPlaceResponseDTO wantPlaceResponseDTOS = wantPlaceService.getRoomList(userPrincipal, id);
+        return ResponseEntity.ok(wantPlaceResponseDTOS);
+    }
 
     @PostMapping("/want-place")
     @ResponseStatus(HttpStatus.OK)
--- src/main/java/com/travel/role/domain/room/service/WantPlaceService.java
+++ src/main/java/com/travel/role/domain/room/service/WantPlaceService.java
@@ -2,10 +2,13 @@ package com.travel.role.domain.room.service;
 
 import com.travel.role.domain.room.dao.RoomRepository;
 import com.travel.role.domain.room.dao.WantPlaceRepository;
+import com.travel.role.domain.room.domain.ParticipantRole;
 import com.travel.role.domain.room.domain.Room;
 import com.travel.role.domain.room.domain.RoomParticipant;
 import com.travel.role.domain.room.domain.WantPlace;
+import com.travel.role.domain.room.dto.WantPlaceDTO;
 import com.travel.role.domain.room.dto.WantPlaceRequestDTO;
+import com.travel.role.domain.room.dto.WantPlaceResponseDTO;
 import com.travel.role.domain.user.dao.UserRepository;
 import com.travel.role.domain.user.domain.User;
 import com.travel.role.domain.user.exception.RoomInfoNotFoundException;
@@ -16,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Set;
+import java.util.*;
 
 import static com.travel.role.global.exception.ExceptionMessage.*;
 
@@ -24,16 +27,36 @@ import static com.travel.role.global.exception.ExceptionMessage.*;
 @RequiredArgsConstructor
 @Transactional
 public class WantPlaceService {
-
+    private static final String PARTICIPANT = "isParticipant";
+    private static final String SCHEDULE = "isSchedule";
     private final UserRepository userRepository;
     private final RoomRepository roomRepository;
     private final WantPlaceRepository wantPlaceRepository;
 
+    public WantPlaceResponseDTO getRoomList(UserPrincipal userPrincipal, Long id) {
+        long idx = 1;
+        User loginUser = findUser(userPrincipal);
+        Room room = findRoom(id);
+        Map<String, Boolean> map = checkParticipant(loginUser, room.getRoomParticipants());
+
+        if (Boolean.TRUE.equals(map.get(PARTICIPANT))) {
+            List<WantPlace> wantPlaces = wantPlaceRepository.findByRoomIdWithRole(id);
+            List<WantPlaceDTO> wantPlaceDTOS = new ArrayList<>();
+            for (WantPlace wantPlace : wantPlaces) {
+                wantPlaceDTOS.add(WantPlaceDTO.of(idx++, wantPlace));
+            }
+            return WantPlaceResponseDTO.of(wantPlaceDTOS, map.get(SCHEDULE));
+        }
+        else
+            throw new UserNotParticipateRoomException(USER_NOT_PARTICIPATE_ROOM);
+    }
+
     public void addWantPlace(UserPrincipal userPrincipal, WantPlaceRequestDTO wantPlaceRequestDTO) {
         User loginUser = findUser(userPrincipal);
         Room room = findRoom(wantPlaceRequestDTO.getRoomId());
-        boolean isParticipant = checkParticipant(loginUser, room.getRoomParticipants());
-        if (isParticipant)
+        Map<String, Boolean> map = checkParticipant(loginUser, room.getRoomParticipants());
+
+        if (Boolean.TRUE.equals(map.get(PARTICIPANT)))
             wantPlaceRepository.save(WantPlace.of(room, wantPlaceRequestDTO));
         else
             throw new UserNotParticipateRoomException(USER_NOT_PARTICIPATE_ROOM);
@@ -49,12 +72,22 @@ public class WantPlaceService {
                 .orElseThrow(() -> new UserInfoNotFoundException(USERNAME_NOT_FOUND));
     }
 
-    private boolean checkParticipant(User loginUser, Set<RoomParticipant> participants) {
-        for (RoomParticipant participant : participants) {
-            if (loginUser.getId().equals(participant.getUser().getId())) {
-                return true;
+    private Map<String, Boolean> checkParticipant(User loginUser, Set<RoomParticipant> participants) {
+        Map<String, Boolean> map = new HashMap<>();
+        map.put(SCHEDULE, false);
+        map.put(PARTICIPANT, false);
+
+        for (RoomParticipant roomParticipant : participants) {
+            if (loginUser.getId().equals(roomParticipant.getUser().getId())) {
+                for (ParticipantRole participantRole : roomParticipant.getParticipantRoles()) {
+                    if (participantRole.getRoomRole().getValue().equals("SCHEDULE")) {
+                        map.replace(SCHEDULE, true);
+                    }
+                }
+                map.replace(PARTICIPANT, true);
             }
         }
-        return false;
+        return map;
     }
+
 }
