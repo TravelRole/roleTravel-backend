a52c3217d0da1f1402111fba0dd80ef737e49173
kwYoohae <gocksdl7979@gmail.com>
2023-04-17 13:49:57 +0900

    feat: roomId 컬럼 추가

--- src/main/java/com/travel/role/domain/room/dto/RoomResponseDTO.java
+++ src/main/java/com/travel/role/domain/room/dto/RoomResponseDTO.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoomResponseDTO {
+	private Long roomId;
 	private String roomName;
 	private LocalDate startDate;
 	private LocalDate endDate;
@@ -20,7 +21,7 @@ public class RoomResponseDTO {
 	private List<MemberDTO> members;
 
 	public static RoomResponseDTO of(Room room, List<MemberDTO> members) {
-		return new RoomResponseDTO(room.getRoomName(), room.getTravelStartDate(), room.getTravelEndDate(),
+		return new RoomResponseDTO(room.getId(), room.getRoomName(), room.getTravelStartDate(), room.getTravelEndDate(),
 			room.getLocation(), members);
 	}
 }
