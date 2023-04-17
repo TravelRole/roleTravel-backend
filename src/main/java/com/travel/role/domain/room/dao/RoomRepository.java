375bd327868cfdc6b6649fffadb6e756d7bf8746
kwYoohae <gocksdl7979@gmail.com>
2023-04-17 22:32:25 +0900

    feat: invite code에 같은 값이 있는지 확인

--- src/main/java/com/travel/role/domain/room/dao/RoomRepository.java
+++ src/main/java/com/travel/role/domain/room/dao/RoomRepository.java
@@ -1,15 +1,18 @@
 package com.travel.role.domain.room.dao;
 
-import com.travel.role.domain.room.domain.Room;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import com.travel.role.domain.room.domain.Room;
 
 public interface RoomRepository extends JpaRepository<Room, Long>, RoomQuerydsl {
     @Query("SELECT r FROM Room r JOIN FETCH r.roomParticipants rp JOIN FETCH rp.user WHERE r.id = :roomId")
     Optional<Room> findByIdWithParticipants(@Param("roomId") Long roomId);
+
+    boolean existsByRoomInviteCode(String inviteCode);
 }
 
 
