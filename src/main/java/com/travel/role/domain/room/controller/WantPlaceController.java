0667d7608439b49a78b45db2a834ee655b1db514
Icln <gksmfthsus35@naver.com>
2023-04-17 14:13:55 +0900

    fix: 매개변수 명 수정

--- src/main/java/com/travel/role/domain/room/controller/WantPlaceController.java
+++ src/main/java/com/travel/role/domain/room/controller/WantPlaceController.java
@@ -20,8 +20,8 @@ public class WantPlaceController {
     private final WantPlaceService wantPlaceService;
 
     @GetMapping("/want-place")
-    public ResponseEntity<WantPlaceResponseDTO> getPlaceList(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam("id") Long id) {
-        WantPlaceResponseDTO wantPlaceResponseDTOS = wantPlaceService.getPlaceList(userPrincipal, id);
+    public ResponseEntity<WantPlaceResponseDTO> getPlaceList(@AuthenticationPrincipal UserPrincipal userPrincipal, @RequestParam("roomId") Long roomId) {
+        WantPlaceResponseDTO wantPlaceResponseDTOS = wantPlaceService.getPlaceList(userPrincipal, roomId);
         return ResponseEntity.ok(wantPlaceResponseDTOS);
     }
 
