11d974c8eff6aca266382d5d14fee5a5b84e4908
kwYoohae <gocksdl7979@gmail.com>
2023-04-17 22:42:18 +0900

    test: 해당 정보의 권한이 없거나 쿼리로 가져온 결과가 없을경우

--- src/test/java/com/travel/role/domain/room/service/RoomServiceTest.java
+++ src/test/java/com/travel/role/domain/room/service/RoomServiceTest.java
@@ -4,6 +4,7 @@ import static com.travel.role.global.exception.ExceptionMessage.*;
 import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -13,8 +14,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.travel.role.domain.room.dao.RoomRepository;
+import com.travel.role.domain.room.domain.RoomRole;
 import com.travel.role.domain.room.dto.MakeRoomRequestDTO;
 import com.travel.role.domain.room.exception.InvalidLocalDateException;
+import com.travel.role.domain.room.exception.UserHaveNotPrivilegeException;
 import com.travel.role.domain.user.dao.UserRepository;
 import com.travel.role.domain.user.exception.UserInfoNotFoundException;
 import com.travel.role.global.auth.token.UserPrincipal;
@@ -28,6 +32,9 @@ class RoomServiceTest {
 
 	@Mock
 	private UserRepository userRepository;
+
+	@Mock
+	private RoomRepository roomRepository;
 
 	@Test
 	void 시작날짜가_종료날짜보다_클_경우() {
@@ -51,6 +58,18 @@ class RoomServiceTest {
 		Assertions.assertThatThrownBy(() -> roomService.makeRoom(makeUserPrincipal(), getMakeRoomRequestDTO()))
 			.isInstanceOf(UserInfoNotFoundException.class)
 			.hasMessageContaining(USERNAME_NOT_FOUND);
+	}
+
+	@Test
+	void 해당_정보의_권한이_없거나_쿼리로_가져온_결과가_없을경우() {
+		// given
+		given(roomRepository.getRoomRole(anyString(), anyLong()))
+			.willReturn(new ArrayList<RoomRole>());
+
+		// when
+		Assertions.assertThatThrownBy(() -> roomService.makeInviteCode(makeUserPrincipal(), 1L))
+			.isInstanceOf(UserHaveNotPrivilegeException.class)
+			.hasMessageContaining(USER_HAVE_NOT_PRIVILEGE);
 	}
 
 	private static MakeRoomRequestDTO getMakeRoomRequestDTO() {
