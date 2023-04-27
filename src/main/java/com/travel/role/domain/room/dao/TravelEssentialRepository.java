e17d6a1d589e58e48294111b6c6ca8ddfdde1880
guswls159357 <guswls159357@naver.com>
2023-04-27 15:31:04 +0900

    feat: 준비물 삭제와 체크시 사용할 쿼리 작성

---
+++ src/main/java/com/travel/role/domain/room/dao/TravelEssentialRepository.java
@@ -0,0 +1,27 @@
+package com.travel.role.domain.room.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.travel.role.domain.room.domain.TravelEssentials;
+
+public interface TravelEssentialRepository extends JpaRepository<TravelEssentials, Long>, TravelEssentialQuerydsl {
+
+	@Modifying
+	@Transactional
+	@Query("DELETE FROM TravelEssentials te "
+		+ "WHERE te.user.id = :userId AND te.room.id = :roomId AND te.id in :ids")
+	void deleteByUserAndRoomAndSearchIds(@Param("userId") Long userId, @Param("roomId") Long roomId,
+		@Param("ids") List<Long> ids);
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE TravelEssentials te SET te.isChecked = :check WHERE te.user.id = :userId AND te.room.id = :roomId AND te.id in :ids")
+	void updateCheckEssentialsByUserAndRoomAndEssentialIds(@Param("userId") Long userId, @Param("roomId") Long roomId,
+		@Param("ids") List<Long> ids, @Param("check") Boolean check);
+}
