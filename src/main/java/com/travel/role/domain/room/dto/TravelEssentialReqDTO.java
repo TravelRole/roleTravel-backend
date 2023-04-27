0c15985ea245563189eee879ab8bcb0cab684ef1
guswls159357 <guswls159357@naver.com>
2023-04-27 15:28:06 +0900

    feat: 준비물 생성시 사용할 요청 DTO 생성
    준비물 생성시에는 List 형태로 준비물 목록을 받아야 하는데,
    이 때 List 내부를 검증하기 위해서 따로 객체를 만들어
    해당 객체 내부에 검증 어노테이션을 선언하고, List 가 있는 DTO 에서는
    @Valid 를 통해 검증이 되도록 구현

---
+++ src/main/java/com/travel/role/domain/room/dto/TravelEssentialReqDTO.java
@@ -0,0 +1,43 @@
+package com.travel.role.domain.room.dto;
+
+import static com.travel.role.global.exception.ExceptionMessage.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.travel.role.domain.room.domain.EssentialCategory;
+import com.travel.role.domain.room.domain.Room;
+import com.travel.role.domain.room.domain.TravelEssentials;
+import com.travel.role.domain.user.domain.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class TravelEssentialReqDTO {
+
+	@NotNull(message = INVALID_ESSENTIAL_CATEGORY)
+	private EssentialCategory category;
+
+	@Valid
+	private List<TravelEssentialItemDTO> items;
+
+	public List<TravelEssentials> toTravelEssentials(User user, Room room) {
+
+		return this.items.stream()
+			.map(item -> TravelEssentials.builder()
+				.user(user)
+				.room(room)
+				.category(this.category)
+				.isChecked(false)
+				.itemName(item.getItem())
+				.build()
+			).collect(Collectors.toList());
+	}
+}
