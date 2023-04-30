68403ebc8f8610f36f47f1641639140cf25a3daa
guswls159357 <guswls159357@naver.com>
2023-04-30 15:27:30 +0900

    fix: 직렬화시 한글 이름을 내려주기 위한 수정
    @JsonValue 를 사용하여 Json 으로 직렬화시 한글 형태로 직렬화 되도록 수정
    
    추가적으로 작성한 user 정보를 반환해야 하고, 누구에게 단 댓글인지,
    삭제된 댓글인지 얼려주기 위한 필드 추가

--- src/main/java/com/travel/role/domain/room/entity/RoomRole.java
+++ src/main/java/com/travel/role/domain/room/entity/RoomRole.java
@@ -1,16 +1,24 @@
 package com.travel.role.domain.room.entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public enum RoomRole {
-	ADMIN("ADMIN"),
-	SCHEDULE("SCHEDULE"),
-	ACCOUNTING("ACCOUNTING"),
-	RESERVATION("RESERVATION"),
-	NONE("NONE");
+	ADMIN("ADMIN", "총무"),
+	SCHEDULE("SCHEDULE", "일정"),
+	ACCOUNTING("ACCOUNTING", "회계"),
+	RESERVATION("RESERVATION", "예약"),
+	NONE("NONE", "역할없음");
 
 	private final String value;
+	private final String responseValue;
+
+	@JsonValue
+	public String getResponseValue() {
+		return this.responseValue;
+	}
 }
