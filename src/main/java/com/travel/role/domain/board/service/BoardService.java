ad341646e83bfd243ce184457807b110ecf93108
Icln <gksmfthsus35@naver.com>
2023-05-02 01:52:50 +0900

    feat: 서비스 작성

--- src/main/java/com/travel/role/domain/board/service/BoardService.java
+++ src/main/java/com/travel/role/domain/board/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.travel.role.domain.board.service;
 
 import com.travel.role.domain.board.dto.request.BoardRequestDTO;
+import com.travel.role.domain.board.dto.response.ScheduleInfoResponseDTO;
 import com.travel.role.domain.board.entity.Board;
 import com.travel.role.domain.board.entity.BookInfo;
 import com.travel.role.domain.board.entity.ScheduleInfo;
@@ -13,14 +14,16 @@ import com.travel.role.domain.room.service.RoomReadService;
 import com.travel.role.domain.user.entity.User;
 import com.travel.role.domain.user.service.UserReadService;
 import com.travel.role.global.exception.room.InvalidLocalDateException;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.travel.role.global.exception.dto.ExceptionMessage.INVALID_DATE_ERROR;
+import static com.travel.role.global.exception.dto.ExceptionMessage.*;
 
 @Service
 @RequiredArgsConstructor
@@ -33,6 +36,31 @@ public class BoardService {
     private final ScheduleInfoRepository scheduleInfoRepository;
     private final RoomParticipantReadService roomParticipantReadService;
 
+    public List<ScheduleInfoResponseDTO> getScheduleInfo(String email, Long roomId, LocalDate date){
+        User user = userReadService.findUserByEmailOrElseThrow(email);
+
+        Room room = roomReadService.findRoomByIdOrElseThrow(roomId);
+
+        roomParticipantReadService.checkParticipant(user, room);
+
+        validateDate(room.getTravelStartDate(), room.getTravelEndDate() , date);
+
+        return getScheduleInfoResult(boardRepository.findBoardBookInfoScheduleInfoByRoomIdAndScheduleDate(roomId, date.atStartOfDay(), date.atTime(LocalTime.MAX)));
+    }
+
+    private List<ScheduleInfoResponseDTO> getScheduleInfoResult(List<Object[]> boardInfos) {
+        List<ScheduleInfoResponseDTO> result = new ArrayList<>();
+
+        for (Object[] objects : boardInfos){
+            Board board = (Board) objects[0];
+            BookInfo bookInfo = (BookInfo) objects[1];
+            ScheduleInfo scheduleInfo = (ScheduleInfo)objects[2];
+
+            result.add(ScheduleInfoResponseDTO.of(board,scheduleInfo,bookInfo));
+        }
+        return result;
+    }
+
     public void addSchedule(String email, BoardRequestDTO boardRequestDTO) {
         User user = userReadService.findUserByEmailOrElseThrow(email);
 
@@ -40,7 +68,7 @@ public class BoardService {
 
         roomParticipantReadService.checkParticipant(user, room);
 
-        validateDate(room, boardRequestDTO);
+        validateDate(room.getTravelStartDate(), room.getTravelEndDate() ,boardRequestDTO.getScheduleDate().toLocalDate());
 
         Board board = boardRepository.save(Board.of(room, boardRequestDTO));
 
@@ -50,14 +78,11 @@ public class BoardService {
             bookInfoRepository.save(BookInfo.from(board));
     }
 
-    public void validateDate(Room room, BoardRequestDTO boardRequestDTO) {
-        LocalDate start = room.getTravelStartDate();
-        LocalDate end = room.getTravelEndDate();
-        LocalDate addTime = boardRequestDTO.getScheduleDate().toLocalDate();
-
-        if (addTime.isBefore(start))
-            throw new InvalidLocalDateException(INVALID_DATE_ERROR);
-        if (addTime.isAfter(end))
-            throw new InvalidLocalDateException(INVALID_DATE_ERROR);
+    public void validateDate(LocalDate startDate, LocalDate endDate, LocalDate date) {
+        if (date.isBefore(startDate))
+            throw new InvalidLocalDateException(EARLY_DATE_ERROR);
+        if (date.isAfter(endDate))
+            throw new InvalidLocalDateException(LATE_DATE_ERROR);
     }
+
 }
