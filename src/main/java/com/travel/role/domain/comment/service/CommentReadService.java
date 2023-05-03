280a05308b4c313b1c2bc12607ac8df0d0979eda
guswls159357 <guswls159357@naver.com>
2023-05-03 20:44:00 +0900

    fix: 댓글 조회시 리턴 타입 변경

--- src/main/java/com/travel/role/domain/comment/service/CommentReadService.java
+++ src/main/java/com/travel/role/domain/comment/service/CommentReadService.java
@@ -1,7 +1,7 @@
 package com.travel.role.domain.comment.service;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,9 +19,9 @@ public class CommentReadService {
 
 	private final CommentRepository commentRepository;
 
-	public List<CommentResDTO> getAllCommentsByRoomId(Long roomId) {
+	public Page<CommentResDTO> getCommentsByRoomId(Long roomId, Pageable pageable) {
 
-		return commentRepository.findAllOrderByGroupIdAndCreateDate(roomId);
+		return commentRepository.findAllOrderByGroupIdAndCreateDate(roomId, pageable);
 	}
 
 	public Comment findCommentByIdOrElseThrow(Long commentId) {
