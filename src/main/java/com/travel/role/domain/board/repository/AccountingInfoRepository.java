3c27f000034b5c0fd25a65ae11c6dcd6f5f6b936
Icln <gksmfthsus35@naver.com>
2023-05-09 01:45:18 +0900

    feat: AccountingInfo 엔티티, 레포지토리 추가

---
+++ src/main/java/com/travel/role/domain/board/repository/AccountingInfoRepository.java
@@ -0,0 +1,7 @@
+package com.travel.role.domain.board.repository;
+
+import com.travel.role.domain.board.entity.AccountingInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountingInfoRepository extends JpaRepository<AccountingInfo, Long> {
+}
