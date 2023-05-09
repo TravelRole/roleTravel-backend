411a3baafbc4b13a0e2e7b96b88fa20a10846602
Icln <gksmfthsus35@naver.com>
2023-05-10 02:46:13 +0900

    fix: import 수정

--- src/main/java/com/travel/role/domain/board/entity/AccountingInfo.java
+++ src/main/java/com/travel/role/domain/board/entity/AccountingInfo.java
@@ -1,27 +1,10 @@
 package com.travel.role.domain.board.entity;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
 import com.travel.role.domain.room.entity.Room;
+import lombok.*;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Table(name = "accounting_info")
 @Getter
