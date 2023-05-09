7d80b0f271f1d6360d8901ce0e2329b688e28942
guswls159357 <67905075+guswls159357@users.noreply.github.com>
2023-05-09 22:25:37 +0900

    Feat #112 여행 경비 수정 기능 (#113)
    * feat: 공동 경비를 수정하기 위한 DTO 생성
    
    * feat: 공동 경비의 입력이 0 보다 작은 경우 발생시킬 예외 작성
    
    * feat: 공동 경비의 입력이 0 보다 작은 경우 발생시킬 예외 메시지 작성
    
    * feat: Room Entity 에 여행 경비 필드 추가 및 변경 메소드 작성
    
    * feat: 여행 경비를 수정하는 서비스 메서드 작성
    
    * feat: 여행 경비를 수정하는 컨트롤러 메서드 작성
    
    * fix: 여행 경비를 총무도 변경할 수 있도록 수정

--- src/main/java/com/travel/role/domain/room/entity/Room.java
+++ src/main/java/com/travel/role/domain/room/entity/Room.java
@@ -26,6 +26,9 @@ public class Room extends BaseCreateTime {
 	@Column(name = "room_name", nullable = false)
 	private String roomName;
 
+	@Column(name = "travel_expense", nullable = false)
+	private int travelExpense;
+
 	@Column(name = "tarvel_start_date", nullable = false)
 	private LocalDate travelStartDate;
 
@@ -44,6 +47,19 @@ public class Room extends BaseCreateTime {
 	@Column(name = "room_expired_time")
 	private LocalDateTime roomExpiredTime;
 
+	public Room(Long id, String roomName, LocalDate travelStartDate, LocalDate travelEndDate, Long roomImage,
+		String location, String roomInviteCode, LocalDateTime roomExpiredTime) {
+		this.id = id;
+		this.roomName = roomName;
+		this.travelExpense = 0;
+		this.travelStartDate = travelStartDate;
+		this.travelEndDate = travelEndDate;
+		this.roomImage = roomImage;
+		this.location = location;
+		this.roomInviteCode = roomInviteCode;
+		this.roomExpiredTime = roomExpiredTime;
+	}
+
 	public static Room of(MakeRoomRequestDTO makeRoomRequestDTO) {
 		return Room.builder()
 			.location(makeRoomRequestDTO.getLocation())
@@ -57,5 +73,9 @@ public class Room extends BaseCreateTime {
 	public void updateInviteCode(String inviteCode, LocalDateTime now) {
 		this.roomInviteCode = inviteCode;
 		this.roomExpiredTime = now;
+	}
+
+	public void updateTravelExpense(int travelExpense) {
+		this.travelExpense = travelExpense;
 	}
 }
--- src/main/java/com/travel/role/domain/room/service/RoomService.java
+++ src/main/java/com/travel/role/domain/room/service/RoomService.java
@@ -1,6 +1,7 @@
 package com.travel.role.domain.room.service;
 
 import com.querydsl.core.Tuple;
+import com.travel.role.domain.room.dto.request.ExpensesRequestDTO;
 import com.travel.role.domain.room.dto.request.MakeRoomRequestDTO;
 import com.travel.role.domain.room.dto.response.InviteResponseDTO;
 import com.travel.role.domain.room.dto.response.MemberDTO;
@@ -20,7 +21,9 @@ import com.travel.role.global.exception.room.InvalidInviteCode;
 import com.travel.role.global.exception.room.InvalidLocalDateException;
 import com.travel.role.global.exception.room.UserHaveNotPrivilegeException;
 import com.travel.role.global.util.PasswordGenerator;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,171 +40,184 @@ import static com.travel.role.global.exception.dto.ExceptionMessage.INVALID_DATE
 @Transactional
 public class RoomService {
 
-    private static final int MAX_PASSWORD_LENGTH = 20;
+	private static final int MAX_PASSWORD_LENGTH = 20;
 
-    private final RoomRepository roomRepository;
-    private final UserReadService userReadService;
-    private final RoomParticipantRepository roomParticipantRepository;
-    private final ParticipantRoleRepository participantRoleRepository;
-    private final PasswordGenerator passwordGenerator;
-    private final RoomReadService roomReadService;
+	private final RoomRepository roomRepository;
+	private final UserReadService userReadService;
+	private final RoomParticipantRepository roomParticipantRepository;
+	private final ParticipantRoleRepository participantRoleRepository;
+	private final PasswordGenerator passwordGenerator;
+	private final RoomReadService roomReadService;
 
-    public List<RoomResponseDTO> getRoomList(String email) {
-        List<Tuple> findRoomInfo = roomRepository.getMemberInRoom(email);
+	public List<RoomResponseDTO> getRoomList(String email) {
+		List<Tuple> findRoomInfo = roomRepository.getMemberInRoom(email);
 
-        Map<Long, RoomResponseDTO> hash = new HashMap<>();
-        for (Tuple tuple : findRoomInfo) {
-            Room room = tuple.get(0, Room.class);
-            User user = tuple.get(1, User.class);
+		Map<Long, RoomResponseDTO> hash = new HashMap<>();
+		for (Tuple tuple : findRoomInfo) {
+			Room room = tuple.get(0, Room.class);
+			User user = tuple.get(1, User.class);
 
-            if (Objects.equals(user.getEmail(), email)) {
-                List<MemberDTO> members = new ArrayList<>();
-                hash.put(room.getId(), RoomResponseDTO.of(room, members));
-            }
-        }
+			if (Objects.equals(user.getEmail(), email)) {
+				List<MemberDTO> members = new ArrayList<>();
+				hash.put(room.getId(), RoomResponseDTO.of(room, members));
+			}
+		}
 
-        for (Tuple tuple : findRoomInfo) {
-            Room room = tuple.get(0, Room.class);
-            User user = tuple.get(1, User.class);
+		for (Tuple tuple : findRoomInfo) {
+			Room room = tuple.get(0, Room.class);
+			User user = tuple.get(1, User.class);
 
-            if (hash.containsKey(room.getId())) {
-                RoomResponseDTO roomResponseDTO = hash.get(room.getId());
-                roomResponseDTO.getMembers().add(new MemberDTO(user.getName(), user.getProfile()));
-                hash.put(room.getId(), roomResponseDTO);
-            }
-        }
+			if (hash.containsKey(room.getId())) {
+				RoomResponseDTO roomResponseDTO = hash.get(room.getId());
+				roomResponseDTO.getMembers().add(new MemberDTO(user.getName(), user.getProfile()));
+				hash.put(room.getId(), roomResponseDTO);
+			}
+		}
 
-        return new ArrayList<>(hash.values());
-    }
+		return new ArrayList<>(hash.values());
+	}
 
-    public void makeRoom(String email, MakeRoomRequestDTO makeRoomRequestDTO) {
-        validateDate(makeRoomRequestDTO);
-        User user = userReadService.findUserByEmailOrElseThrow(email);
-        Room room = roomRepository.save(Room.of(makeRoomRequestDTO));
-        saveNewRoomParticipant(user, room);
-        saveNewParticipantRole(user, room);
-    }
+	public void makeRoom(String email, MakeRoomRequestDTO makeRoomRequestDTO) {
+		validateDate(makeRoomRequestDTO);
+		User user = userReadService.findUserByEmailOrElseThrow(email);
+		Room room = roomRepository.save(Room.of(makeRoomRequestDTO));
+		saveNewRoomParticipant(user, room);
+		saveNewParticipantRole(user, room);
+	}
 
-    private void saveNewRoomParticipant(User user, Room room) {
-        RoomParticipant roomParticipant = RoomParticipant.builder()
-                .isPaid(false)
-                .joinedAt(LocalDateTime.now())
-                .room(room)
-                .user(user)
-                .build();
-        roomParticipantRepository.save(roomParticipant);
-    }
+	private void saveNewRoomParticipant(User user, Room room) {
+		RoomParticipant roomParticipant = RoomParticipant.builder()
+			.isPaid(false)
+			.joinedAt(LocalDateTime.now())
+			.room(room)
+			.user(user)
+			.build();
+		roomParticipantRepository.save(roomParticipant);
+	}
 
-    private void saveNewParticipantRole(User user, Room room) {
-        ParticipantRole newParticipantRole = ParticipantRole.builder()
-                .roomRole(RoomRole.ADMIN)
-                .room(room)
-                .user(user)
-                .build();
-        participantRoleRepository.save(newParticipantRole);
-    }
+	private void saveNewParticipantRole(User user, Room room) {
+		ParticipantRole newParticipantRole = ParticipantRole.builder()
+			.roomRole(RoomRole.ADMIN)
+			.room(room)
+			.user(user)
+			.build();
+		participantRoleRepository.save(newParticipantRole);
+	}
 
-    private void validateDate(MakeRoomRequestDTO makeRoomRequestDTO) {
-        LocalDate start = makeRoomRequestDTO.getTravelStartDate();
-        LocalDate end = makeRoomRequestDTO.getTravelEndDate();
+	private void validateDate(MakeRoomRequestDTO makeRoomRequestDTO) {
+		LocalDate start = makeRoomRequestDTO.getTravelStartDate();
+		LocalDate end = makeRoomRequestDTO.getTravelEndDate();
 
-        if (start.isAfter(end))
-            throw new InvalidLocalDateException(INVALID_DATE_ERROR);
-    }
+		if (start.isAfter(end))
+			throw new InvalidLocalDateException(INVALID_DATE_ERROR);
+	}
 
-    public String makeInviteCode(String email, Long roomId) {
-        User user = userReadService.findUserByEmailOrElseThrow(email);
-        Room room = roomReadService.findRoomByIdOrElseThrow(roomId);
+	public String makeInviteCode(String email, Long roomId) {
+		User user = userReadService.findUserByEmailOrElseThrow(email);
+		Room room = roomReadService.findRoomByIdOrElseThrow(roomId);
 
-        validRoomRole(user, room, RoomRole.ADMIN);
+		validRoomRole(user, room, RoomRole.ADMIN);
 
-        String inviteCode = room.getRoomInviteCode();
-        if (validateInviteCode(room)) {
-            inviteCode = generateInviteCode();
-            room.updateInviteCode(inviteCode, LocalDateTime.now());
-        }
+		String inviteCode = room.getRoomInviteCode();
+		if (validateInviteCode(room)) {
+			inviteCode = generateInviteCode();
+			room.updateInviteCode(inviteCode, LocalDateTime.now());
+		}
 
-        return inviteCode;
-    }
+		return inviteCode;
+	}
 
-    private boolean validateInviteCode(Room room) {
-        return room.getRoomInviteCode() == null || room.getRoomExpiredTime().plusDays(1L).isAfter(LocalDateTime.now());
-    }
+	private boolean validateInviteCode(Room room) {
+		return room.getRoomInviteCode() == null || room.getRoomExpiredTime().plusDays(1L).isAfter(LocalDateTime.now());
+	}
 
-    private void validRoomRole(User user, Room room, RoomRole... checkRooms) {
-        boolean isExist = participantRoleRepository.existsByUserAndRoomAndRoomRoleIn(user, room, Arrays.asList(checkRooms));
-        if (!isExist)
-            throw new UserHaveNotPrivilegeException();
-    }
+	private void validRoomRole(User user, Room room, RoomRole... checkRooms) {
+		boolean isExist = participantRoleRepository.existsByUserAndRoomAndRoomRoleIn(user, room,
+			Arrays.asList(checkRooms));
+		if (!isExist)
+			throw new UserHaveNotPrivilegeException();
+	}
 
-    private String generateInviteCode() {
-        String inviteCode = passwordGenerator.generateRandomPassword(MAX_PASSWORD_LENGTH);
+	private String generateInviteCode() {
+		String inviteCode = passwordGenerator.generateRandomPassword(MAX_PASSWORD_LENGTH);
 
-        while (roomRepository.existsByRoomInviteCode(inviteCode)) {
-            inviteCode = passwordGenerator.generateRandomPassword(MAX_PASSWORD_LENGTH);
-        }
+		while (roomRepository.existsByRoomInviteCode(inviteCode)) {
+			inviteCode = passwordGenerator.generateRandomPassword(MAX_PASSWORD_LENGTH);
+		}
 
-        return inviteCode;
-    }
+		return inviteCode;
+	}
 
-    @Transactional(readOnly = true)
-    public void checkRoomInviteCode(String email, String inviteCode) {
-        Room room = roomReadService.getRoomUsingInviteCode(inviteCode);
+	@Transactional(readOnly = true)
+	public void checkRoomInviteCode(String email, String inviteCode) {
+		Room room = roomReadService.getRoomUsingInviteCode(inviteCode);
 
-        validateInviteRoom(email, room);
-    }
+		validateInviteRoom(email, room);
+	}
 
-    private void validateInviteRoom(String email, Room room) {
-        if (!validateInviteCode(room)) {
-            throw new InvalidInviteCode();
-        }
+	private void validateInviteRoom(String email, Room room) {
+		if (!validateInviteCode(room)) {
+			throw new InvalidInviteCode();
+		}
 
 		if (roomParticipantRepository.existsUserInRoom(email, room.getId())) {
 			throw new AlreadyExistInRoomException();
 		}
 	}
 
-    public InviteResponseDTO inviteUser(String email, String inviteCode, List<String> roles) {
-        Room room = roomReadService.getRoomUsingInviteCode(inviteCode);
-        User user = userReadService.findUserByEmailOrElseThrow(email);
+	public InviteResponseDTO inviteUser(String email, String inviteCode, List<String> roles) {
+		Room room = roomReadService.getRoomUsingInviteCode(inviteCode);
+		User user = userReadService.findUserByEmailOrElseThrow(email);
 
-        validateInviteRoom(email, room);
-        validateSelectRole(roles);
+		validateInviteRoom(email, room);
+		validateSelectRole(roles);
 
-        for (String role : roles) {
-            ParticipantRole participantRole = new ParticipantRole(null, RoomRole.valueOf(role), user, room);
-            participantRoleRepository.save(participantRole);
-        }
+		for (String role : roles) {
+			ParticipantRole participantRole = new ParticipantRole(null, RoomRole.valueOf(role), user, room);
+			participantRoleRepository.save(participantRole);
+		}
 
-        RoomParticipant roomParticipant = new RoomParticipant(null, LocalDateTime.now(), false, user, room);
-        roomParticipantRepository.save(roomParticipant);
+		RoomParticipant roomParticipant = new RoomParticipant(null, LocalDateTime.now(), false, user, room);
+		roomParticipantRepository.save(roomParticipant);
 
-        return new InviteResponseDTO(room.getId());
-    }
+		return new InviteResponseDTO(room.getId());
+	}
 
-    private void validateSelectRole(List<String> roles) {
-        for (String role : roles) {
-            try {
-                if (RoomRole.valueOf(role) == RoomRole.ADMIN) {
-                    throw new UserHaveNotPrivilegeException();
-                }
-            } catch (IllegalArgumentException e) {
-                throw new UserHaveNotPrivilegeException();
-            }
-        }
-    }
+	private void validateSelectRole(List<String> roles) {
+		for (String role : roles) {
+			try {
+				if (RoomRole.valueOf(role) == RoomRole.ADMIN) {
+					throw new UserHaveNotPrivilegeException();
+				}
+			} catch (IllegalArgumentException e) {
+				throw new UserHaveNotPrivilegeException();
+			}
+		}
+	}
 
-    public List<TimeResponseDTO> getTime(Long roomId) {
-        Room room = roomReadService.findRoomByIdOrElseThrow(roomId);
-        LocalDate starDate = room.getTravelStartDate();
-        LocalDate endDate = room.getTravelEndDate();
-        Long day = ChronoUnit.DAYS.between(starDate, endDate);
-        List<TimeResponseDTO> result = new ArrayList<>();
+	public List<TimeResponseDTO> getTime(Long roomId) {
+		Room room = roomReadService.findRoomByIdOrElseThrow(roomId);
+		LocalDate starDate = room.getTravelStartDate();
+		LocalDate endDate = room.getTravelEndDate();
+		Long day = ChronoUnit.DAYS.between(starDate, endDate);
+		List<TimeResponseDTO> result = new ArrayList<>();
 
-        for (Long i = 1L; i <= day + 1; i++) {
-            result.add(TimeResponseDTO.from(i, starDate, starDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN)));
-            starDate = starDate.plusDays(1);
-        }
-        return result;
-    }
+		for (Long i = 1L; i <= day + 1; i++) {
+			result.add(TimeResponseDTO.from(i, starDate,
+				starDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN)));
+			starDate = starDate.plusDays(1);
+		}
+		return result;
+	}
+
+	public void modifyExpenses(String email, Long roomId, ExpensesRequestDTO requestDTO) {
+
+		User loginUser = userReadService.findUserByEmailOrElseThrow(email);
+		Room room = roomReadService.findRoomByIdOrElseThrow(roomId);
+
+		validRoomRole(loginUser, room, RoomRole.ACCOUNTING, RoomRole.ADMIN);
+
+		room.updateTravelExpense(requestDTO.getExpenses());
+	}
+
 }
