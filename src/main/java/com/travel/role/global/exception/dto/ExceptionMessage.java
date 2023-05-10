55dc50ec8fe0e3515672141448fb5ffb1a162f4e
guswls159357 <guswls159357@naver.com>
2023-05-11 01:26:43 +0900

    feat: 결제 시간을 잘못 입력시 발생시킬 예외 메시지 작성

--- src/main/java/com/travel/role/global/exception/dto/ExceptionMessage.java
+++ src/main/java/com/travel/role/global/exception/dto/ExceptionMessage.java
@@ -61,4 +61,5 @@ public final class ExceptionMessage {
 	public static final String INVALID_PAYMENT_PRICE = "금액은 1원 이상이어야 합니다.";
 	public static final String INVALID_CATEGORY = "지정된 카테고리 내에서 입력해 주세요.";
 	public static final String INVALID_PAYMENT_METHOD = "지정된 결제수단 내에서 입력해 주세요";
+	public static final String INVALID_PAYMENT_TIME = "결제 시간을 yyyy-MM-dd HH:mm 형식으로 올바른 시간을 입력해 주세요.";
 }
