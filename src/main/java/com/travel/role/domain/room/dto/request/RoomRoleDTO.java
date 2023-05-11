ef3b2e71cdef64a9d7142beab995fcaa25ddca41
kwYoohae <gocksdl7979@gmail.com>
2023-05-11 19:25:30 +0900

    fix: 필요없는 valid 로직 수정

--- src/main/java/com/travel/role/domain/room/dto/request/RoomRoleDTO.java
+++ src/main/java/com/travel/role/domain/room/dto/request/RoomRoleDTO.java
@@ -2,9 +2,6 @@ package com.travel.role.domain.room.dto.request;
 
 import java.util.List;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-
 import com.travel.role.domain.room.entity.RoomRole;
 
 import lombok.AllArgsConstructor;
@@ -15,8 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoomRoleDTO {
-	@Email
-	@NotEmpty
 	private String email;
 	private List<RoomRole> roles;
 }
