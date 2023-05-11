4854af8603b511a3fbc9bfa8296ed50d6b86b49a
guswls159357 <guswls159357@naver.com>
2023-05-12 00:51:57 +0900

    feat: 지출 내역을 가져오는 쿼리 작성
    결제 수단이 null 인 경우는 전체를 조회하도록 동적쿼리 작성

---
+++ src/main/java/com/travel/role/domain/accounting/repository/querydsl/AccountingInfoQuerydsl.java
@@ -0,0 +1,12 @@
+package com.travel.role.domain.accounting.repository.querydsl;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.travel.role.domain.accounting.entity.AccountingInfo;
+import com.travel.role.domain.accounting.entity.PaymentMethod;
+
+public interface AccountingInfoQuerydsl {
+
+	List<AccountingInfo> findAllByRoomIdAndDateAndPaymentMethod(Long roomId, LocalDate date, PaymentMethod paymentMethod);
+}
