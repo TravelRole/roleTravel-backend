c21a7623eede28693a8455b6775fd42bca63c22f
kwYoohae <gocksdl7979@gmail.com>
2023-05-12 17:59:59 +0900

    feat: RoomInfo에 대한 responseDTO 작성

---
+++ src/main/java/com/travel/role/domain/room/dto/response/RoomInfoResponseDTO.java
@@ -0,0 +1,23 @@
+package com.travel.role.domain.room.dto.response;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.travel.role.domain.room.dto.request.RoomRoleDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomInfoResponseDTO {
+	private String roomName;
+	@JsonFormat(pattern = "yyyy/MM/dd")
+	private LocalDate startDate;
+	@JsonFormat(pattern = "yyyy/MM/dd")
+	private LocalDate endDate;
+	private List<RoomRoleDTO> roles;
+}
