53ad347e9bb67119a7dd4320f7092cf8c4d4435c
Icln <gksmfthsus35@naver.com>
2023-05-15 13:28:28 +0900

    feat: 쿼리추가

--- src/main/java/com/travel/role/domain/board/repository/BoardRepository.java
+++ src/main/java/com/travel/role/domain/board/repository/BoardRepository.java
@@ -1,21 +1,36 @@
 package com.travel.role.domain.board.repository;
 
-import com.travel.role.domain.board.entity.Board;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import com.travel.role.domain.board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    @Query(value = "SELECT DISTINCT b, ai, si, bi " +
-            "FROM Board b " +
-            "JOIN FETCH b.accountingInfo ai " +
-            "JOIN FETCH b.scheduleInfo si " +
-            "JOIN FETCH ai.bookInfo bi " +
-            "WHERE b.room.id = :roomId " +
-            "AND b.scheduleDate BETWEEN :startOfDay AND :endOfDay")
-    List<Board> findBoardByRoomIdAndScheduleDate
-            (@Param("roomId") Long roomId, @Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
+	@Query(value = "SELECT DISTINCT b, ai, si, bi " +
+		"FROM Board b " +
+		"JOIN FETCH b.accountingInfo ai " +
+		"JOIN FETCH b.scheduleInfo si " +
+		"JOIN FETCH ai.bookInfo bi " +
+		"WHERE b.room.id = :roomId " +
+		"AND b.scheduleDate BETWEEN :startOfDay AND :endOfDay " +
+		"ORDER BY b.scheduleDate ASC")
+	List<Board> findBoardByRoomIdAndScheduleDate
+		(@Param("roomId") Long roomId, @Param("startOfDay") LocalDateTime startOfDay,
+			@Param("endOfDay") LocalDateTime endOfDay);
+
+	@Query(value = "SELECT DISTINCT b, ai, si, bi " +
+		"FROM Board b " +
+		"LEFT JOIN FETCH b.accountingInfo ai " +
+		"JOIN FETCH b.scheduleInfo si " +
+		"LEFT JOIN FETCH ai.bookInfo bi " +
+		"WHERE b.room.id = :roomId " +
+		"AND b.scheduleDate BETWEEN :startOfDay AND :endOfDay " +
+		"ORDER BY b.scheduleDate ASC")
+	List<Board> findScheduleByRoomIdAndScheduleDate
+		(@Param("roomId") Long roomId, @Param("startOfDay") LocalDateTime startOfDay,
+			@Param("endOfDay") LocalDateTime endOfDay);
 }
