4a8d85399c9bbf50a1b369870a243f4fc14559a7
Icln <gksmfthsus35@naver.com>
2023-05-15 13:26:34 +0900

    refactor: 들여쓰기 수정

--- src/main/java/com/travel/role/domain/board/dto/response/BookInfoResponseDTO.java
+++ src/main/java/com/travel/role/domain/board/dto/response/BookInfoResponseDTO.java
@@ -1,46 +1,50 @@
 package com.travel.role.domain.board.dto.response;
 
+import java.time.LocalTime;
+
 import com.travel.role.domain.accounting.entity.AccountingInfo;
 import com.travel.role.domain.accounting.entity.Category;
 import com.travel.role.domain.accounting.entity.PaymentMethod;
-import com.travel.role.domain.board.entity.*;
+import com.travel.role.domain.board.entity.Board;
+import com.travel.role.domain.board.entity.BookInfo;
+import com.travel.role.domain.schedule.entity.ScheduleInfo;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalTime;
 
 @Getter
 @NoArgsConstructor
 public class BookInfoResponseDTO {
-    private String placeName;
-    private LocalTime time;
-    private String link;
-    private String bookEtc;
-    private Integer price;
-    private PaymentMethod paymentMethod;
-    private Category category;
-    private String accountingEtc;
-    private Boolean isBooked;
-    private Long bookInfoId;
-    private Long accountingId;
+	private String placeName;
+	private LocalTime time;
+	private String link;
+	private String bookEtc;
+	private Integer price;
+	private PaymentMethod paymentMethod;
+	private Category category;
+	private String accountingEtc;
+	private Boolean isBooked;
+	private Long bookInfoId;
+	private Long accountingId;
 
-    private BookInfoResponseDTO(Board board, ScheduleInfo scheduleInfo, AccountingInfo accountingInfo, BookInfo bookInfo) {
-        this.placeName = scheduleInfo.getPlaceName();
-        this.time = board.getScheduleDate().toLocalTime();
-        this.link = board.getLink();
-        this.bookEtc = bookInfo.getBookEtc();
-        this.price = accountingInfo.getPrice();
-        this.paymentMethod = accountingInfo.getPaymentMethod();
-        this.category = board.getCategory();
-        this.accountingEtc = accountingInfo.getAccountingEtc();
-        this.isBooked = bookInfo.getIsBooked();
-        this.bookInfoId = bookInfo.getId();
-        this.accountingId = accountingInfo.getId();
-    }
+	private BookInfoResponseDTO(Board board, ScheduleInfo scheduleInfo, AccountingInfo accountingInfo,
+		BookInfo bookInfo) {
+		this.placeName = scheduleInfo.getPlaceName();
+		this.time = board.getScheduleDate().toLocalTime();
+		this.link = board.getLink();
+		this.bookEtc = bookInfo.getBookEtc();
+		this.price = accountingInfo.getPrice();
+		this.paymentMethod = accountingInfo.getPaymentMethod();
+		this.category = board.getCategory();
+		this.accountingEtc = accountingInfo.getAccountingEtc();
+		this.isBooked = bookInfo.getIsBooked();
+		this.bookInfoId = bookInfo.getId();
+		this.accountingId = accountingInfo.getId();
+	}
 
-    public static BookInfoResponseDTO of(Board board, ScheduleInfo scheduleInfo, AccountingInfo accountingInfo,
-                                         BookInfo bookInfo) {
-        return new BookInfoResponseDTO(board, scheduleInfo, accountingInfo, bookInfo);
-    }
+	public static BookInfoResponseDTO of(Board board, ScheduleInfo scheduleInfo, AccountingInfo accountingInfo,
+		BookInfo bookInfo) {
+		return new BookInfoResponseDTO(board, scheduleInfo, accountingInfo, bookInfo);
+	}
 
 }
--- src/main/java/com/travel/role/domain/board/entity/Board.java
+++ src/main/java/com/travel/role/domain/board/entity/Board.java
@@ -1,13 +1,32 @@
 package com.travel.role.domain.board.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 import com.travel.role.domain.accounting.entity.AccountingInfo;
 import com.travel.role.domain.accounting.entity.Category;
 import com.travel.role.domain.board.dto.request.BoardRequestDTO;
 import com.travel.role.domain.room.entity.Room;
-import lombok.*;
+import com.travel.role.domain.schedule.entity.ScheduleInfo;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "board")
 @Getter
@@ -17,39 +36,39 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Board {
 
-    @Id
-    @Column(name = "board_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@Column(name = "board_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "room_id", nullable = false, updatable = false)
-    private Room room;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id", nullable = false, updatable = false)
+	private Room room;
 
-    @Column(name = "schedule_date")
-    private LocalDateTime scheduleDate;
+	@Column(name = "schedule_date")
+	private LocalDateTime scheduleDate;
 
-    @Column
-    private String link;
+	@Column
+	private String link;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 100)
-    private Category category;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 100)
+	private Category category;
 
-    @OneToOne(mappedBy = "board", cascade = CascadeType.ALL)
-    private ScheduleInfo scheduleInfo;
+	@OneToOne(mappedBy = "board", cascade = CascadeType.ALL)
+	private ScheduleInfo scheduleInfo;
 
-    @OneToOne(mappedBy = "board", cascade = CascadeType.ALL)
-    private AccountingInfo accountingInfo;
+	@OneToOne(mappedBy = "board", cascade = CascadeType.ALL)
+	private AccountingInfo accountingInfo;
 
-    private Board(Room room, BoardRequestDTO boardRequestDTO) {
-        this.room = room;
-        this.scheduleDate = boardRequestDTO.getScheduleDate();
-        this.link = boardRequestDTO.getLink();
-        this.category = boardRequestDTO.getCategory();
-    }
+	private Board(Room room, BoardRequestDTO boardRequestDTO) {
+		this.room = room;
+		this.scheduleDate = boardRequestDTO.getScheduleDate();
+		this.link = boardRequestDTO.getLink();
+		this.category = boardRequestDTO.getCategory();
+	}
 
-    public static Board of(Room room, BoardRequestDTO boardRequestDTO) {
-        return new Board(room, boardRequestDTO);
-    }
+	public static Board of(Room room, BoardRequestDTO boardRequestDTO) {
+		return new Board(room, boardRequestDTO);
+	}
 }
