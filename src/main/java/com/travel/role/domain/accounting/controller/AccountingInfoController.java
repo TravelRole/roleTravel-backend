cbae6e7681b4cf9dbcf4ca85e5f70cdd9fdfb93f
guswls159357 <67905075+guswls159357@users.noreply.github.com>
2023-05-15 14:05:25 +0900

    Feat #144 지출 내역 삭제 (#145)
    * feat: 지출 내역 삭제 서비스 메서드 생성
    
    * feat: 지출 내역 삭제 컨트롤러 메서드 생성

--- src/main/java/com/travel/role/domain/accounting/controller/AccountingInfoController.java
+++ src/main/java/com/travel/role/domain/accounting/controller/AccountingInfoController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,5 +72,14 @@ public class AccountingInfoController {
 			userPrincipal.getEmail(), roomId, accountingId, requestDTO);
 
 		return ResponseEntity.ok(resDTO);
+	}
+
+	@DeleteMapping("/{accounting_id}")
+	public void deleteExpenseDetail(
+		@AuthenticationPrincipal UserPrincipal userPrincipal,
+		@PathVariable("roomId") Long roomId,
+		@PathVariable("accounting_id") Long accountingId){
+
+		accountingInfoService.deleteExpenseDetail(userPrincipal.getEmail(), roomId, accountingId);
 	}
 }
