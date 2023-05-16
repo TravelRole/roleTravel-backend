27a2cf57d6738ca968ca210251d00737041e813a
Icln <gksmfthsus35@naver.com>
2023-05-17 02:18:21 +0900

    feat: DTO 작성

---
+++ src/main/java/com/travel/role/domain/schedule/dto/request/ScheduleRequestDTO.java
@@ -0,0 +1,48 @@
+package com.travel.role.domain.schedule.dto.request;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.travel.role.domain.accounting.entity.Category;
+import com.travel.role.global.exception.dto.ExceptionMessage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleRequestDTO {
+
+	private Long boardId;
+
+	@NotBlank(message = ExceptionMessage.PLACE_NAME_VALUE_NOT_EMPTY)
+	private String placeName;
+
+	@NotBlank(message = ExceptionMessage.PLACE_ADDRESS_VALUE_NOT_EMPTY)
+	private String placeAddress;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+	private LocalDateTime scheduleDate;
+
+	private String link;
+
+	private Boolean isBookRequired;
+
+	private Category category;
+
+	@NotNull(message = ExceptionMessage.LATITUDE_VALUE_NOT_EMPTY)
+	private Double latitude;
+
+	@NotNull(message = ExceptionMessage.LONGITUDE_NOT_EMPTY)
+	private Double longitude;
+
+	private String etc;
+
+	@NotNull(message = ExceptionMessage.MAP_PLACE_ID_VALUE_NOT_EMPTY)
+	private Long mapPlaceId;
+}
