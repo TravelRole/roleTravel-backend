b06a6a75c38c6daa59ccc40fdb3f77decb23af65
Icln <gksmfthsus35@naver.com>
2023-05-16 14:42:18 +0900

    fix: 쿼리명 변경

--- src/main/java/com/travel/role/domain/wantplace/repository/WantPlaceRepository.java
+++ src/main/java/com/travel/role/domain/wantplace/repository/WantPlaceRepository.java
@@ -1,16 +1,17 @@
 package com.travel.role.domain.wantplace.repository;
 
-import com.travel.role.domain.wantplace.entity.WantPlace;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import com.travel.role.domain.wantplace.entity.WantPlace;
 
 public interface WantPlaceRepository extends JpaRepository<WantPlace, Long> {
-    @Query("SELECT DISTINCT w " +
-            "FROM WantPlace w " +
-            "JOIN FETCH w.room r " +
-            "WHERE r.id = :roomId ORDER BY w.createDate ASC")
-    List<WantPlace> findByRoomIdWithRole(@Param("roomId") Long roomId);
+	@Query("SELECT DISTINCT w " +
+		"FROM WantPlace w " +
+		"JOIN FETCH w.room r " +
+		"WHERE r.id = :roomId ORDER BY w.createDate ASC")
+	List<WantPlace> findWantPlaceByRoomId(@Param("roomId") Long roomId);
 }
