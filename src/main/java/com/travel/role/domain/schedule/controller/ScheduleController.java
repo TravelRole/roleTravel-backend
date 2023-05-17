bcd279fdc9715a2b8bfb841aca2c6a7f0629af35
Icln <gksmfthsus35@naver.com>
2023-05-17 16:13:55 +0900

    fix: 메소드 방식 수정

--- src/main/java/com/travel/role/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/travel/role/domain/schedule/controller/ScheduleController.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,7 +55,7 @@ public class ScheduleController {
 		scheduleService.deleteSchedule(userPrincipal.getEmail(), roomId, scheduleIds);
 	}
 
-	@PutMapping
+	@PatchMapping
 	public void modifySchedule(@AuthenticationPrincipal UserPrincipal userPrincipal,
 		@PathVariable("roomId") Long roomId,
 		@RequestBody @Valid ScheduleModifyRequestDTO scheduleModifyRequestDTO) {
