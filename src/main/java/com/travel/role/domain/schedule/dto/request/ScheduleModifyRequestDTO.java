342d51d3f18036a240655e416b06e642fdd507b2
Icln <gksmfthsus35@naver.com>
2023-05-17 16:13:05 +0900

    feat: DTO 생성 및 유효성 검사 추가

--- src/main/java/com/travel/role/domain/schedule/dto/request/ScheduleModifyRequestDTO.java
+++ src/main/java/com/travel/role/domain/schedule/dto/request/ScheduleModifyRequestDTO.java
@@ -16,32 +16,21 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ScheduleRequestDTO {
+public class ScheduleModifyRequestDTO {
+
+	@NotNull(message = ExceptionMessage.BOARD_ID_EMPTY)
+	private Long boardId;
 
 	@NotBlank(message = ExceptionMessage.PLACE_NAME_VALUE_NOT_EMPTY)
 	private String placeName;
 
-	@NotBlank(message = ExceptionMessage.PLACE_ADDRESS_VALUE_NOT_EMPTY)
-	private String placeAddress;
-
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
 	private LocalDateTime scheduleDate;
-
-	private String link;
 
 	private Boolean isBookRequired;
 
 	@NotNull(message = ExceptionMessage.INVALID_CATEGORY)
 	private Category category;
 
-	@NotNull(message = ExceptionMessage.LATITUDE_VALUE_NOT_EMPTY)
-	private Double latitude;
-
-	@NotNull(message = ExceptionMessage.LONGITUDE_NOT_EMPTY)
-	private Double longitude;
-
 	private String etc;
-
-	@NotNull(message = ExceptionMessage.MAP_PLACE_ID_VALUE_NOT_EMPTY)
-	private Long mapPlaceId;
 }
