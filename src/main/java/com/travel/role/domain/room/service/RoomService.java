77261296fe318de0c8408ee34c5e18e175793708
kwYoohae <gocksdl7979@gmail.com>
2023-05-20 15:47:33 +0900

    feat: room 삭제 로직 작성

--- src/main/java/com/travel/role/domain/room/service/RoomService.java
+++ src/main/java/com/travel/role/domain/room/service/RoomService.java
@@ -541,6 +541,7 @@ public class RoomService {
 		boardRepository.deleteAllByRoomId(roomId);
 		participantRoleRepository.deleteAllByRoomId(roomId);
 		roomParticipantRepository.deleteAllByRoomId(roomId);
+
 	}
 
 	private List<Long> getBookIds(List<AccountingInfo> accountingInfos) {
