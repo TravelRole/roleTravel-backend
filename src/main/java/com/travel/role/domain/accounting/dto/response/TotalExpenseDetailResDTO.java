345710f70a5721130b91a2f17e50f194eb58a929
guswls159357 <guswls159357@naver.com>
2023-05-28 16:16:39 +0900

    feat: 총 지출 금액 조회시 사용할 DTO 작성

---
+++ src/main/java/com/travel/role/domain/accounting/dto/response/TotalExpenseDetailResDTO.java
@@ -0,0 +1,13 @@
+package com.travel.role.domain.accounting.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class TotalExpenseDetailResDTO {
+
+	private Integer totalExpense;
+}
