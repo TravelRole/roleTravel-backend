72f52940b48db2d9346f6cfd4d332714e583e4ec
guswls159357 <guswls159357@naver.com>
2023-05-29 16:48:01 +0900

    feat: 준비물 삭제, 체크 Controller 테스트 작성

--- src/test/java/com/travel/role/unit/travelessential/controller/TravelEssentialControllerTest.java
+++ src/test/java/com/travel/role/unit/travelessential/controller/TravelEssentialControllerTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -25,11 +24,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.travel.role.domain.travelessential.dto.request.TravelEssentialCheckReqDTO;
+import com.travel.role.domain.travelessential.dto.request.TravelEssentialDeleteReqDTO;
 import com.travel.role.domain.travelessential.dto.request.TravelEssentialItemDTO;
 import com.travel.role.domain.travelessential.dto.request.TravelEssentialReqDTO;
 import com.travel.role.domain.travelessential.dto.response.TravelEssentialResDTO;
@@ -41,7 +41,6 @@ import com.travel.role.domain.user.entity.User;
 import com.travel.role.domain.user.repository.UserRepository;
 import com.travel.role.global.auth.entity.AuthInfo;
 import com.travel.role.global.auth.repository.AuthRepository;
-import com.travel.role.global.auth.token.UserPrincipal;
 import com.travel.role.global.exception.dto.ExceptionMessage;
 import com.travel.role.global.exception.user.RoomInfoNotFoundException;
 import com.travel.role.global.exception.user.UserNotParticipateRoomException;
@@ -106,7 +105,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 				.createTravelEssentials(anyString(), anyLong(), any(TravelEssentialReqDTO.class));
 		}
 
-		@DisplayName("존재하지 않는 방이면 예외를 발생시킨다.")
+		@DisplayName("존재하지 않는 방이면 예외메시지를 응답한다.")
 		@Test
 		void createTravelEssentialsWithNotExistsRoom() throws Exception {
 
@@ -130,7 +129,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 				.andExpect(jsonPath("$.time").exists());
 		}
 
-		@DisplayName("방에 속해 있지 않은 회원이면 예외를 발생시킨다.")
+		@DisplayName("방에 속해 있지 않은 회원이면 예외메시지를 응답한다.")
 		@Test
 		void createTravelEssentialsWithNotParticipantUser() throws Exception {
 
@@ -154,7 +153,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 				.andExpect(jsonPath("$.time").exists());
 		}
 
-		@DisplayName("카테고리를 잘못 입력했다면 예외를 발생시킨다.")
+		@DisplayName("카테고리를 잘못 입력했다면 예외메시지를 응답한다.")
 		@NullAndEmptySource
 		@CsvSource({"invalid"})
 		@ParameterizedTest
@@ -178,7 +177,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 			then(travelEssentialService).shouldHaveNoInteractions();
 		}
 
-		@DisplayName("준비물 이름을 잘못 입력했다면 예외를 발생시킨다.")
+		@DisplayName("준비물 이름을 잘못 입력했다면 예외메시지를 응답한다.")
 		@EmptySource
 		@CsvSource({"aaaaa.aaaaa.aaaa"})
 		@ParameterizedTest
@@ -202,7 +201,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 			then(travelEssentialService).shouldHaveNoInteractions();
 		}
 
-		@DisplayName("준비물 이름을 입력하지 않았다면 예외를 발생시킨다.")
+		@DisplayName("준비물 이름을 입력하지 않았다면 예외메시지를 응답한다.")
 		@Test
 		void createTravelEssentialsWithEmptyItemName() throws Exception {
 
@@ -259,7 +258,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 			then(travelEssentialReadService).should(times(1)).readAllGroupByCategory(anyString(), anyLong());
 		}
 
-		@DisplayName("존재하지 않는 방이면 예외를 발생시킨다.")
+		@DisplayName("존재하지 않는 방이면 예외메시지를 응답한다.")
 		@Test
 		void readTravelEssentialsWithNotExistsRoom() throws Exception {
 
@@ -276,7 +275,7 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 				.andExpect(jsonPath("$.time").exists());
 		}
 
-		@DisplayName("방에 속해 있지 않은 회원이면 예외를 발생시킨다.")
+		@DisplayName("방에 속해 있지 않은 회원이면 예외메시지를 응답한다.")
 		@Test
 		void readTravelEssentialsWithNotParticipantUser() throws Exception {
 
@@ -293,6 +292,174 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 				.andExpect(jsonPath("$.time").exists());
 		}
 
+		@DisplayName("회원이 준비물을 삭제했을 때")
+		@Nested
+		class DeleteTravelEssentials {
+
+			@DisplayName("정상적인 경우라면 준비물이 삭제된다.")
+			@Test
+			void deleteTravelEssentials() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialDeleteReqDTO request = createTravelEssentialDeleteReqDTO(requestIds);
+
+				//When & Then
+				mockMvc.perform(delete("/api/room/1/essentials")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isOk());
+
+				then(travelEssentialService).should(times(1))
+					.deleteTravelEssentials(anyString(), anyLong(), any(TravelEssentialDeleteReqDTO.class));
+			}
+
+			@DisplayName("존재하지 않는 방이면 예외메시지를 응답한다.")
+			@Test
+			void deleteTravelEssentialsWithNotExistsRoom() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialDeleteReqDTO request = createTravelEssentialDeleteReqDTO(requestIds);
+
+				// Stub
+				RoomInfoNotFoundException expectedException = new RoomInfoNotFoundException();
+				willThrow(expectedException).given(travelEssentialService)
+					.deleteTravelEssentials(anyString(), anyLong(), any(TravelEssentialDeleteReqDTO.class));
+
+				//When & Then
+				mockMvc.perform(delete("/api/room/1/essentials")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isNotFound())
+					.andExpect(jsonPath("$.message").value(ExceptionMessage.ROOM_NOT_FOUND))
+					.andExpect(jsonPath("$.httpStatus").value(HttpStatus.NOT_FOUND.name()))
+					.andExpect(jsonPath("$.time").exists());
+			}
+
+			@DisplayName("방에 속해 있지 않은 회원이면 예외메시지를 응답한다.")
+			@Test
+			void deleteTravelEssentialsWithNotParticipantUser() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialDeleteReqDTO request = createTravelEssentialDeleteReqDTO(requestIds);
+
+				// Stub
+				UserNotParticipateRoomException expectedException = new UserNotParticipateRoomException();
+				willThrow(expectedException).given(travelEssentialService)
+					.deleteTravelEssentials(anyString(), anyLong(), any(TravelEssentialDeleteReqDTO.class));
+
+				//When & Then
+				mockMvc.perform(delete("/api/room/1/essentials")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isUnauthorized())
+					.andExpect(jsonPath("$.message").value(ExceptionMessage.USER_NOT_PARTICIPATE_ROOM))
+					.andExpect(jsonPath("$.httpStatus").value(HttpStatus.UNAUTHORIZED.name()))
+					.andExpect(jsonPath("$.time").exists());
+			}
+		}
+
+		@DisplayName("회원이 준비물을 체크했을 때")
+		@Nested
+		class CheckTravelEssentials {
+
+			@DisplayName("정상적인 경우라면 준비물이 체크된다.")
+			@Test
+			void checkTravelEssentials() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialCheckReqDTO request = createTravelEssentialCheckReqDTO(true,
+					requestIds);
+
+				//When & Then
+				mockMvc.perform(patch("/api/room/1/essentials/check")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isOk());
+
+				then(travelEssentialService).should(times(1))
+					.updateCheckTravelEssentials(anyString(), anyLong(), any(TravelEssentialCheckReqDTO.class));
+			}
+
+			@DisplayName("존재하지 않는 방이면 예외메시지를 응답한다.")
+			@Test
+			void checkTravelEssentialsWithNotExistsRoom() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialCheckReqDTO request = createTravelEssentialCheckReqDTO(true,
+					requestIds);
+
+				// Stub
+				RoomInfoNotFoundException expectedException = new RoomInfoNotFoundException();
+				willThrow(expectedException).given(travelEssentialService)
+					.updateCheckTravelEssentials(anyString(), anyLong(), any(TravelEssentialCheckReqDTO.class));
+
+				//When & Then
+				mockMvc.perform(patch("/api/room/1/essentials/check")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isNotFound())
+					.andExpect(jsonPath("$.message").value(ExceptionMessage.ROOM_NOT_FOUND))
+					.andExpect(jsonPath("$.httpStatus").value(HttpStatus.NOT_FOUND.name()))
+					.andExpect(jsonPath("$.time").exists());
+			}
+
+			@DisplayName("방에 속해 있지 않은 회원이면 예외메시지를 응답한다.")
+			@Test
+			void checkTravelEssentialsWithNotParticipantUser() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialCheckReqDTO request = createTravelEssentialCheckReqDTO(true,
+					requestIds);
+
+				// Stub
+				UserNotParticipateRoomException expectedException = new UserNotParticipateRoomException();
+				willThrow(expectedException).given(travelEssentialService)
+					.updateCheckTravelEssentials(anyString(), anyLong(), any(TravelEssentialCheckReqDTO.class));
+
+				//When & Then
+				mockMvc.perform(patch("/api/room/1/essentials/check")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isUnauthorized())
+					.andExpect(jsonPath("$.message").value(ExceptionMessage.USER_NOT_PARTICIPATE_ROOM))
+					.andExpect(jsonPath("$.httpStatus").value(HttpStatus.UNAUTHORIZED.name()))
+					.andExpect(jsonPath("$.time").exists());
+			}
+
+			@DisplayName("어떤 상태로 변경할 것인지 입력되지 않았다면 예외메시지를 응답한다.")
+			@Test
+			void checkTravelEssentialsWithEmptyStatus() throws Exception {
+
+				// Given
+				List<Long> requestIds = List.of(1L, 2L);
+				TravelEssentialCheckReqDTO request = createTravelEssentialCheckReqDTO(null,
+					requestIds);
+
+				//When & Then
+				mockMvc.perform(patch("/api/room/1/essentials/check")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(om.writeValueAsString(request))
+						.characterEncoding("UTF-8"))
+					.andExpect(status().isBadRequest())
+					.andExpect(jsonPath("$.message").value(ExceptionMessage.ESSENTIAL_CHECK_STATUS_NOT_EMPTY))
+					.andExpect(jsonPath("$.httpStatus").value(HttpStatus.BAD_REQUEST.name()))
+					.andExpect(jsonPath("$.time").exists());
+
+				then(travelEssentialService).shouldHaveNoInteractions();
+			}
+		}
 	}
 
 	private User createMockUser() {
@@ -328,5 +495,15 @@ class TravelEssentialControllerTest extends ControllerTestSupport {
 
 		return Arrays.stream(itemNames).map(TravelEssentialItemDTO::new)
 			.collect(Collectors.toList());
+	}
+
+	private TravelEssentialDeleteReqDTO createTravelEssentialDeleteReqDTO(List<Long> ids) {
+
+		return new TravelEssentialDeleteReqDTO(ids);
+	}
+
+	private TravelEssentialCheckReqDTO createTravelEssentialCheckReqDTO(Boolean check, List<Long> ids) {
+
+		return new TravelEssentialCheckReqDTO(check, ids);
 	}
 }
